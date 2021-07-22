d111112571a123e2ed54fe5989661e2b666e3e61
kouta1612 <kouta1612world69@gmail.com>
2021-07-22 22:55:06 +0900

    use alias

--- src/js/client.js
+++ src/js/client.js
@@ -3,11 +3,9 @@ import ReactDOM from "react-dom";
 
 class Layout extends React.Component {
   render() {
-    return (
-      <h1>Welcome, kota.hagiwara!</h1>
-    );
+    return <h1>Welcome!</h1>;
   }
 }
 
-const app = document.getElementById('app');
-ReactDOM.render(<Layout/>, app);
+const app = document.getElementById("app");
+ReactDOM.render(<Layout />, app);
